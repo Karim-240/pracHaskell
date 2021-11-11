cf72d14579590b18863f1bd078bac29965afb4af
Kareem <karimmisael@gmail.com>
2021-11-11 15:31:12 -0600

    Continuacion

--- codificador.hs
+++ codificador.hs
@@ -1,51 +1,55 @@
+-- ================================================
+--                     Declaraciones
+-- ================================================
 import Data.Char
 import Data.Bits
 type Bit = Int  
 
-
- -- ================================================
- --                     Conversion
- -- ================================================
-
--- Recibe valor binario y convierte a entero ascii --
-bin2int :: [Bit] -> Int
-bin2int =  foldr (\x y -> x + 2*y) 0
- 
--- Recibe valor entero ascii y convierte a binario --
-int2bin :: Int -> [Bit]
-int2bin n | n < 2     = [n]
-          | otherwise = n `mod` 2 : int2bin (n `div` 2)
- 
- -- ================================================
- --                     Codificacion
+-- ================================================
+ --                     Encriptación
  -- ================================================
  
--- Complementa valor binario a un octeto --
-creaOcteto :: [Bit] -> [Bit]
-creaOcteto bs =  take 8 (bs ++ repeat 0)
- 
--- Funcion principal, recibe cadena, convierte caracteres en binario, complementa octeto y concatena los octetos 
-palabra ::  [Char] ->  [Bit] 
-palabra  =  concat.map(creaOcteto.int2bin.ord)
-
 encripta :: [Char] -> [Char] -> [Bit]
-encripta pala contra = opXor (palabra pala) (palabra contra)
-
-
-opXor :: [Bit] -> [Bit] -> [Bit]
-opXor  xs ys = zipWith xor xs ys
+encripta pala contra = sentXor (string2bin pala) (string2bin contra)
 
  -- ================================================
- --                    Decodificacion
+ --                    Desencriptación
  -- ================================================
 
- -- Divide los octetos, primera parte de la decodificacion 
-separaOctetos :: [Bit] -> [[Bit]]
-separaOctetos [] = []
-separaOctetos bs =  
-    take 8 bs : separaOctetos (drop 8 bs)
+desencripta :: [Bit] -> [Char] -> String
+desencripta pala contra = traduce (sentXor pala (string2bin contra) )
+
+-- ================================================
+--                     Operaciones
+-- ================================================
+
+-- Recibe  binario y regresa entero --
+b2i :: [Bit] -> Int
+b2i =  foldr (\x y -> x + 2*y) 0
  
--- Funcion principal recibe lista binaria y separa octetos y regresa valor decodificado 
-descodifica :: [Bit] -> String
-descodifica =  map (chr.bin2int).separaOctetos
+-- Recibe entero  y regresa binario --
+i2b :: Int -> [Bit]
+i2b n | n < 2     = [n]
+          | otherwise = n `mod` 2 : i2b (n `div` 2)
  
+-- Recibe cadena y regresa lista de bits
+string2bin ::  [Char] ->  [Bit] 
+string2bin  =  concat.map(creaOct.i2b.ord)
+
+-- Complementa valor binario a un octeto --
+creaOct :: [Bit] -> [Bit]
+creaOct bs =  take 8 (bs ++ repeat 0)
+
+-- Divide los octetos
+separaOct :: [Bit] -> [[Bit]]
+separaOct [] = []
+separaOct bs =  
+    take 8 bs : separaOct (drop 8 bs)
+
+-- Aplica sentencia Xor a dos listas binarias 
+sentXor :: [Bit] -> [Bit] -> [Bit]
+sentXor  xs ys = zipWith xor xs ys
+ 
+-- Recibe lista binaria y convierte a String
+traduce  :: [Bit] -> String
+traduce  =  map (chr.b2i).separaOct
